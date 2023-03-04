9b7341f65e08428bad25d592280805f79b68a405
eden2292 <kp798@student.aru.ac.uk>
2023-03-04 18:48:14 +0000

    Completed staff home page

--- LibraryAssignment/StaffHome.xaml.cs
+++ LibraryAssignment/StaffHome.xaml.cs
@@ -23,5 +23,53 @@ namespace LibraryAssignment
         {
             InitializeComponent();
         }
+        private void btnLogOut_Click(object sender, RoutedEventArgs e)
+        {
+            Hide();
+            MainWindow main = new MainWindow();
+            main.Show();
+        }
+
+        #region Label_Controls
+        private void btnManageBooks_MouseEnter(object sender, MouseEventArgs e)
+        {
+            lblManageBooksInfo.Visibility = Visibility.Visible;
+        }
+
+        private void btnManageBooks_MouseLeave(object sender, MouseEventArgs e)
+        {
+            lblManageBooksInfo.Visibility = Visibility.Hidden;
+        }
+
+        private void btnManageMembers_MouseEnter(object sender, MouseEventArgs e)
+        {
+            lblManageMembersInfo.Visibility = Visibility.Visible;
+        }
+
+        private void btnManageMembers_MouseLeave(object sender, MouseEventArgs e)
+        {
+            lblManageMembersInfo.Visibility = Visibility.Hidden;
+        }
+
+        private void btnReports_MouseEnter(object sender, MouseEventArgs e)
+        {
+            lblReportsInfo.Visibility = Visibility.Visible;
+        }
+
+        private void btnReports_MouseLeave(object sender, MouseEventArgs e)
+        {
+            lblReportsInfo.Visibility = Visibility.Hidden;
+        }
+        #endregion
+
+
+
+
+
+
+
+
+
+
     }
 }
