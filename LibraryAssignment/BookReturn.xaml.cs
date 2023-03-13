ff23ad3bf3079587648c3e5bc71fc96368e602f6
eden2292 <kp798@student.aru.ac.uk>
2023-03-13 11:44:42 +0000

    renew book, ReplaceChile() issue

--- LibraryAssignment/BookReturn.xaml.cs
+++ LibraryAssignment/BookReturn.xaml.cs
@@ -8,7 +8,7 @@ namespace LibraryAssignment
     /// Interaction logic for BookReturn.xaml
     /// </summary>
     public partial class BookReturn : Window
-    { 
+    {
         private readonly PramStore _pramStore;
         public String currentUserId;
         public String currentUserBooks;
@@ -16,11 +16,12 @@ namespace LibraryAssignment
 
         private String xmlBookFilePath => "LibraryInventory.xml";
         private String xmlUserFilePath => "UserList.xml";
+        private string newDate = DateTime.Now.AddMonths(1).ToShortDateString();
 
         public BookReturn(PramStore pramStore)
         {
-        _pramStore = pramStore;
-        InitializeComponent();
+            _pramStore = pramStore;
+            InitializeComponent();
             currentUserId = _pramStore.CurrentUser.UserId;
             currentUserBooks = _pramStore.CurrentUser.UserBooks;
         }
@@ -32,6 +33,12 @@ namespace LibraryAssignment
             home.Show();
         }
 
+        private void txtReturn_GotFocus(object sender, RoutedEventArgs e)
+        {
+            txtReturn.Clear();
+        }
+
+        #region return
         private void btnReturn_Click(object sender, RoutedEventArgs e)
         {
             XmlDocument xmlDocument = new XmlDocument();
@@ -65,7 +72,7 @@ namespace LibraryAssignment
             {
                 XmlNode bookTitle = user.SelectSingleNode("/catalog/User/CheckedOut/BookTitle");
 
-                if(bookTitle.InnerText == title)
+                if (bookTitle.InnerText == title)
                 {
                     bookTitle.ParentNode.RemoveChild(bookTitle);
                 }
@@ -75,5 +82,31 @@ namespace LibraryAssignment
             userDocument.Save(xmlUserFilePath);
         }
 
+
+        #endregion
+
+        private void btnRenew_Click(object sender, RoutedEventArgs e)
+        {
+            XmlDocument xmlDocument = new XmlDocument();
+            xmlDocument.Load(xmlBookFilePath);
+
+            XmlNodeList bookNodes = xmlDocument.DocumentElement.SelectNodes("/library/book");
+
+            foreach (XmlNode book in bookNodes)
+            {
+
+                XmlNode bookId = book.SelectSingleNode("bookId");
+
+                if (txtReturn.Text == bookId.InnerText)
+                {
+
+                    XmlNode checkedOut = book.SelectSingleNode("checkedOut");
+                    //title = book.SelectSingleNode("title").InnerText;
+                    XmlNode dueDate("<checkedOut>" + newDate + "</checkedOut>");
+                    book.ReplaceChild(dueDate, checkedOut);
+
+                }
+            }
         }
     }
+}
