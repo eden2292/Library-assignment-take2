29e88abcb90349bb792affdc2e12c8f3b8a0ae1f
eden2292 <KP798@student.aru.ac.uk>
2023-03-23 11:47:37 +0000

    update books in progress

--- LibraryAssignment/Librarian/BookManage.xaml.cs
+++ LibraryAssignment/Librarian/BookManage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Windows;
 using System.Xml;
 
@@ -13,12 +14,24 @@ namespace LibraryAssignment
         public BookManage()
         {
             InitializeComponent();
+
+            DataSet dataset = new DataSet();
+            dataset.ReadXml(xmlBookFilePath);
+            dgLibraryInventory.ItemsSource = dataset.Tables[0].DefaultView;
         }
 
         #region variables
 
         private String xmlBookFilePath => "LibraryInventory.xml";
         private List<BookDetails> books = new List<BookDetails>();
+
+        private String selectTitle;
+        private String selectAuthor;
+        private String selectYear;
+        private String selectPublisher;
+        private String selectIsbn;
+        private String selectCategory;
+        private String selectId;
 
         #endregion variables
 
@@ -61,7 +74,53 @@ namespace LibraryAssignment
 
             xmlDocBooks.Save(xmlBookFilePath);
 
-            MessageBox.Show($"Success. Book ID is\n {id}");
+            MessageBox.Show($"Success. \n Book ID is {id} \n Write in before continuing.");
+
+            DataSet dataset = new DataSet();
+            dataset.ReadXml(xmlBookFilePath);
+            dgLibraryInventory.ItemsSource = dataset.Tables[0].DefaultView;
+            dgLibraryInventory.Items.Refresh();
+        }
+
+        private void dgLibraryInventory_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        {
+            DataRowView row = dgLibraryInventory.SelectedItem as DataRowView;
+
+            selectTitle = row.Row.ItemArray[0].ToString();
+            selectAuthor = row.Row.ItemArray[1].ToString();
+            selectYear = row.Row.ItemArray[2].ToString();
+            selectPublisher = row.Row.ItemArray[3].ToString();
+            selectIsbn = row.Row.ItemArray[4].ToString();
+            selectCategory = row.Row.ItemArray[5].ToString();
+
+            txtManTitle.Text = selectTitle;
+            txtManAuthor.Text = selectAuthor;
+            txtManYear.Text = selectYear;
+            txtManPublisher.Text = selectPublisher;
+            txtManIsbn.Text = selectIsbn;
+            txtManCategory.Text = selectCategory;
+        }
+
+        private void btnManUpdate_Click(object sender, RoutedEventArgs e)
+        {
+            XmlDocument xmlDocBooks = new XmlDocument();
+            xmlDocBooks.Load(xmlBookFilePath);
+
+            XmlNode oldBook = xmlDocBooks.SelectSingleNode("//book[title ='" + selectTitle + "']"); ;
+
+            oldBook.ChildNodes.Item(0).InnerText = txtManTitle.Text;
+            oldBook.ChildNodes.Item(1).InnerText = txtManAuthor.Text;
+            oldBook.ChildNodes.Item(2).InnerText = txtManYear.Text;
+            oldBook.ChildNodes.Item(3).InnerText = txtManPublisher.Text;
+            oldBook.ChildNodes.Item(4).InnerText = txtManIsbn.Text;
+            oldBook.ChildNodes.Item(5).InnerText = txtManCategory.Text;
+
+            MessageBox.Show("Success!");
+
+            DataSet dataset = new DataSet();
+            dataset.ReadXml(xmlBookFilePath);
+            dgLibraryInventory.ItemsSource = dataset.Tables[0].DefaultView;
+            dgLibraryInventory.Items.Refresh();
         }
     }
 }
